252e45ab4c574e5e2d7cf6f01f3e84fdcda1f3fc
snieegu <maxoo97@gmail.com>
2022-04-12 14:02:26 +0200

    small CleanUp

--- isingDataset.py
+++ isingDataset.py
@@ -13,22 +13,3 @@ class IsingDataset(Dataset):
 
     def __len__(self):
         return len(self.data)
-
-    # data = np.load('ising/cfg_x016_b0100.npy')
-    #
-    # def __init__(self, ising_dir, transform=None):
-    #     self.ising_dir = ising_dir
-    #     self.transform = transform
-    #
-    #     all_data = os.listdir(ising_dir)
-    #     self.total_ising_data = all_data
-    #
-    # def __len__(self):
-    #     return len(self.data)
-    #
-    # def __getitem__(self, idx):
-    #     # data_loc = os.path.join(self.ising_dir, self.total_ising_data[idx])
-    #
-    #     ising_data = self.data[idx]
-    #     tensor_data = self.transform(ising_data)
-    #     return tensor_data
